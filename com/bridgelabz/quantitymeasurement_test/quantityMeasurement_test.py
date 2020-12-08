81c6612e3fd49b169f577cfc33b0d134c8398d85
Birajit <birajit95@gmail.com>
2020-12-08 23:14:47 +0530

    [Birajit] Add: Test case to ensure Feet value must be a float type is added

--- com/bridgelabz/quantitymeasurement_test/quantityMeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_test/quantityMeasurement_test.py
@@ -1,4 +1,5 @@
 from com.bridgelabz.quantitymeasurement.quantityMeasurement import Feet
+from com.bridgelabz.quantitymeasurement.InvalidTypeException import InvalidTypeException
 import pytest
 
 
@@ -6,3 +7,8 @@ def test_givenZeroFeetAndZeroFeet_WhenCompared_ShouldReturnTrue():
     feetValue1 = Feet(0.0)
     feetValue2 = Feet(0.0)
     assert feetValue1 == feetValue2
+
+
+def test_givenAFeetValue_IfNotFloatType_ShouldRaise_InvalidTypeException():
+    with pytest.raises(InvalidTypeException):
+        feetValue = Feet(10)
