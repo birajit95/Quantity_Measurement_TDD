18caa5e3f7f5cd95995496de8ab978c75e8d4678
Birajit <birajit95@gmail.com>
2020-12-08 23:40:56 +0530

    [Birajit] Add: Test case to ensure when 2 same values are give the should be of same feet type

--- com/bridgelabz/quantitymeasurement_test/quantityMeasurement_test.py
+++ com/bridgelabz/quantitymeasurement_test/quantityMeasurement_test.py
@@ -12,3 +12,10 @@ def test_givenZeroFeetAndZeroFeet_WhenCompared_ShouldReturnTrue():
 def test_givenAFeetValue_IfNotFloatType_ShouldRaise_InvalidTypeException():
     with pytest.raises(InvalidTypeException):
         feetValue = Feet(10)
+
+
+def test_givenTwoSameValues_WhenComparedIfTypeMissMatched_ShouldRaise_InvalidTypeException():
+    with pytest.raises(InvalidTypeException):
+        feetValue1 = Feet(0.0)
+        feetValue2 = float(0.0)
+        assert feetValue1 == feetValue2
