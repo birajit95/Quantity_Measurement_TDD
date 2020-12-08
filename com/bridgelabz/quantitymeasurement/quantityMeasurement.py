c8696948e453a718bb80a582952f8bbe218c92ac
Birajit <birajit95@gmail.com>
2020-12-09 00:23:14 +0530

    [Birajit] Refactor: Refactored to ensure inch type should be a float feature is developed and tested successfully

--- com/bridgelabz/quantitymeasurement/quantityMeasurement.py
+++ com/bridgelabz/quantitymeasurement/quantityMeasurement.py
@@ -16,10 +16,15 @@ class Feet:
 
 class Inch:
     def __init__(self, inch):
+        if type(inch) is not float:
+            raise InvalidTypeException(ExceptionType.NOT_FLOAT_TYPE_EXCEPTION.value)
         self.inch = inch
 
     def __eq__(self, other):
         return self.inch == other.inch
+
+
+
 
 if __name__ == '__main__':
     print("Welcome to Quantity Measurement Problem")
