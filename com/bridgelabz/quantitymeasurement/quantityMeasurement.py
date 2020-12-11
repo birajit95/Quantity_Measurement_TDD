4f954add9a3c56cdcacf721b016d15ad99890b89
Birajit <birajit95@gmail.com>
2020-12-11 06:37:04 +0530

    [Birajit] Refactor: Till Use Case 3 codes are refactored to achive DRY principle

--- com/bridgelabz/quantitymeasurement/quantityMeasurement.py
+++ com/bridgelabz/quantitymeasurement/quantityMeasurement.py
@@ -1,97 +1,38 @@
 from com.bridgelabz.quantitymeasurement.InvalidTypeException import InvalidTypeException
 from com.bridgelabz.quantitymeasurement.InvalidTypeException import ExceptionType
+import enum
 
 
-class Length:
-    def __init__(self, length):
-        if type(length) is not float and type(length) is not int:
-            raise InvalidTypeException(ExceptionType.NOT_A_NUMBER_TYPE_EXCEPTION.value)
-        self.length = length
-
-    def __eq__(self, other):
-        if type(self) == type(other):
-            return self.length == other.length
-        elif isinstance(self, Feet) and isinstance(other, Inch):
-            return other.length == self.length * 12
-        elif isinstance(self, Inch) and isinstance(other, Feet):
-            return other.length * 12 == self.length
-        elif isinstance(self, Feet) and isinstance(other, Yard):
-            return other.length * 3 == self.length
-        elif isinstance(self, Yard) and isinstance(other, Feet):
-            return other.length == self.length * 3
-        elif isinstance(self, Inch) and isinstance(other, Yard):
-            return other.length * 36 == self.length
-        elif isinstance(self, Yard) and isinstance(other, Inch):
-            return other.length == self.length * 36
-        elif isinstance(self, Inch) and isinstance(other, Centimetre):
-            return other.length == self.length * 2.5
-        elif isinstance(self, Centimetre) and isinstance(other, Inch):
-            return other.length * 2.5 == self.length
-        elif type(self) != type(other) and self.length == other:
-            raise InvalidTypeException(ExceptionType.NOT_LENGTH_TYPE_EXCEPTION.value)
-
-    @staticmethod
-    def convertLength(length1, length2):
-        if isinstance(length1, Feet) and isinstance(length2, Inch):
-            return length1.length * 12
-        if isinstance(length1, Inch) and isinstance(length2, Centimetre):
-            return length2.length / 2.5
-
-
-class Feet(Length):
-    def __init__(self, feet):
-        super().__init__(length=feet)
+class QuantityMeasurement:
+    def __init__(self, unit, value):
+        self.__value = value
+        self.__unit = unit
 
     def __eq__(self, other):
-        return super().__eq__(other)
-
-    def __add__(self, other):
-        if isinstance(other, Inch):
-            convertedLength = Length.convertLength(self, other)
-            other.length = other.length + convertedLength
-            return other
-        self.length = self.length + other.length
-        return self
-
-
-class Inch(Length):
-    def __init__(self, inch):
-        super().__init__(length=inch)
-
-    def __eq__(self, other):
-        return super().__eq__(other)
-
-    def __add__(self, other):
-        if isinstance(other, Centimetre):
-            convertedLength = Length.convertLength(self, other)
-            self.length = self.length + convertedLength
-            return self
-        self.length = self.length + other.length
-        return self
+        if isinstance(other, QuantityMeasurement):
+            if self.__unit == other.__unit and self.__value == other.__value:
+                return True
+            elif self.__unit == other.__unit and self.__value != other.__value:
+                return False
+            elif self.__unit != other.__unit and self.__value == other.__value:
+                return False
+            else:
+                value1, value2 = convert(self.__unit, other.__unit, self.__value, other.__value)
+                return value1 == value2
+        raise InvalidTypeException(ExceptionType.NOT_LENGTH_TYPE_EXCEPTION.value)
 
 
-class Yard(Length):
-    def __init__(self, yard):
-        super().__init__(length=yard)
-
-    def __eq__(self, other):
-        return super().__eq__(other)
-
-    def __add__(self, other):
-        self.length = self.length + other.length
-        return self
+class Length(enum.Enum):
+    Inch = 1.0
+    Feet = 12.0
+    Yard = 36.0
+    Centimetre = 0.4
 
 
-class Centimetre(Length):
-    def __init__(self, cm):
-        super().__init__(length=cm)
-
-    def __eq__(self, other):
-        return super().__eq__(other)
-
-    def __add__(self, other):
-        self.length = self.length + other.length
-        return self
+def convert(value1Unit, value2Unit, value1, value2):
+    value1 = value1Unit.value * value1
+    value2 = value2Unit.value * value2
+    return value1, value2
 
 
 if __name__ == '__main__':
