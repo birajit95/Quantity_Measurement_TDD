e1c0fbf03aba3a0b30564dc039fb29c19ad7a5ad
Birajit <birajit95@gmail.com>
2020-12-12 01:55:21 +0530

    [Birajit] Add: Feature for Weight type is added and all test cases are tested successfully

--- com/bridgelabz/quantitymeasurement/Unit.py
+++ com/bridgelabz/quantitymeasurement/Unit.py
@@ -12,3 +12,9 @@ class Volume(enum.Enum):
     Ml = 1.0  # base for volume
     Liter = 1000.0
     Gallon = 3780.0
+
+
+class Weight(enum.Enum):
+    Gram = 1.0
+    KG = 1000.0
+    Tonne = 1000000
