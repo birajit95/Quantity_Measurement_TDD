c631a752e3341eaf404d7a666531c0a1bed5045a
Birajit <birajit95@gmail.com>
2020-12-11 10:09:18 +0530

    [Birajit] Add: Feature to ensure sum(zero unit of any vol types) = zero unit of any vol type is added and tested successfully

--- com/bridgelabz/quantitymeasurement/quantityMeasurement.py
+++ com/bridgelabz/quantitymeasurement/quantityMeasurement.py
@@ -37,10 +37,13 @@ class QuantityMeasurement:
 
 
 class Length(enum.Enum):
-    Inch = 1.0
+    Inch = 1.0          # base for length
     Feet = 12.0
     Yard = 36.0
     Centimetre = 0.4
+    Ml = 1.0            # base for volume
+    Liter = 1000.0
+    Gallon = 3780.0
 
 
 def convert(value1Unit, value2Unit, value1, value2):
